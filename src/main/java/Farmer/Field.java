9885cfaaca089fbd9f72cf08ea9b7c3058c816a6
MahoganyGant <mahoganygant02@gmail.com>
2024-11-06 19:44:52 -0500

    Completed all assigned team member four work thus far

--- src/main/java/Farmer/Field.java
+++ src/main/java/Farmer/Field.java
@@ -1,4 +1,20 @@
 package Farmer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Field {
+    private List<CropRow> cropRows;
+
+    public Field() {
+        this.cropRows = new ArrayList<>();
+    }
+
+    public void addCropRow(CropRow cropRow) {
+        cropRows.add(cropRow);
+    }
+
+    public List<CropRow> getCropRows() {
+        return cropRows;
+    }
 }
